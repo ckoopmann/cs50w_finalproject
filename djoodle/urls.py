590e21a91e4b47fff719ebfcb6a5ae75c3b64b77
Christian Koopmann <c.k.e.koopmann@gmail.com>
2019-10-12 14:14:56 +0200

    Add everything before cleaning pycache

--- djoodle/urls.py
+++ djoodle/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('survey/', include('survey.urls')),
+    path('', include('survey.urls')),
 ]
--- survey/views.py
+++ survey/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def home(request):
    return HttpResponse('<h1>Welcome to Djoodle</h1>')
+
+def about(request):
+   return HttpResponse('<h1>About Djoodle</h1>')
