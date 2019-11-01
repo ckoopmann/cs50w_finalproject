693a1662e31bc847594b019f9d7826c553ca15dd
Christian Koopmann <c.k.e.koopmann@gmail.com>
2019-11-01 17:20:33 +0100

    Enable adding and deleting surveyoptions

--- survey/urls.py
+++ survey/urls.py
@@ -4,13 +4,17 @@ from .views import (
         SurveyDeleteView,
         SurveyDetailView,
         SurveyListView,
+        SurveyOptionCreateView,
+        SurveyOptionDeleteView,
         SurveyUpdateView
 )
 
 urlpatterns = [
     path('', SurveyListView.as_view(), name='survey-home'),
     path('survey/new/', SurveyCreateView.as_view(), name='survey-create'),
+    path('survey/<int:pk>/newoption/', SurveyOptionCreateView.as_view(), name='surveyoption-create'),
     path('survey/<int:pk>/', SurveyDetailView.as_view(), name='survey-detail'),
     path('survey/<int:pk>/update/', SurveyUpdateView.as_view(), name='survey-update'),
     path('survey/<int:pk>/delete/', SurveyDeleteView.as_view(), name='survey-delete'),
+    path('survey/deleteoption/<int:pk>/', SurveyOptionDeleteView.as_view(), name='surveyoption-delete'),
 ]
--- survey/views.py
+++ survey/views.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import (
     CreateView,
     DeleteView,
     DetailView,
     ListView,
     UpdateView)
-from .models import Survey
+from .models import Survey, SurveyOption
 
 class SurveyListView(ListView):
     model = Survey
@@ -50,3 +51,33 @@ class SurveyUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
             return True
         else:
             return False
+
+
+class SurveyOptionCreateView(LoginRequiredMixin, CreateView):
+    model = SurveyOption
+    fields = ['description']
+
+    def form_valid(self, form):
+        form.instance.survey = Survey.objects.get(pk=self.kwargs['pk'])
+        return super().form_valid(form)
+
+    def get_success_url(self):
+            return reverse('survey-detail', kwargs={'pk': self.kwargs['pk']})
+
+
+class SurveyOptionDeleteView(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
+    model = SurveyOption
+
+    def test_func(self):
+        surveyoption = self.get_object()
+        if self.request.user == surveyoption.survey.author:
+            return True
+        else:
+            return False
+
+    def get_success_url(self):
+        surveyoption = self.get_object()
+        return reverse('survey-detail', kwargs={'pk': surveyoption.survey.id})
+
+
+
